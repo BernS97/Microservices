7d742eadfb168224d6f5c6e463396701b37a0d56
mseitz <moritzseitz97@gmail.com>
2023-01-05 13:06:36 +0100

    Added startup bat

--- startup.bat
+++ startup.bat
@@ -1,8 +1,8 @@
 @echo off
 
-start /wait npm install
-start /wait npm install --prefix services/city
-start /wait npm install --prefix services/weather
-start /wait node server.js &
-start /wait node services/city/server.js &
-start /wait node services/weather/server.js
+start npm install
+start npm install --prefix services/city
+start npm install --prefix services/weather
+start node server.js &
+start node services/city/server.js &
+start node services/weather/server.js
